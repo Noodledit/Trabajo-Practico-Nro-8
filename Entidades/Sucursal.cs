8357a16756f1f600e76ef4504de6824fd0980135
ValenDuarte1918 <valentinduarte1@hotmail.com>
2025-05-30 00:10:39 -0300

    Agregue algunos Gets y Sets en Entidades.

--- Entidades/Sucursal.cs
+++ Entidades/Sucursal.cs
@@ -8,5 +8,33 @@ namespace Entidades
 {
     internal class Sucursal
     {
+        private int IdSucursal;
+        private int IdProvinciaSucursal;
+        private String NombreSucursal;
+
+        public int getIdSucursal()
+        {
+            return IdSucursal;
+        }
+        public void setIdSucursal(int idSucursal)
+        {
+            IdSucursal = idSucursal;
+        }
+        public int getIdProvinciaSucursal()
+        {
+            return IdProvinciaSucursal;
+        }
+        public void setIdProvinciaSucursal(int idProvinciaSucursal)
+        {
+            IdProvinciaSucursal = idProvinciaSucursal;
+        }
+        public String getNombre()
+        {
+            return NombreSucursal;
+        }
+        public void setNombre(string nombre)
+        {
+            NombreSucursal = nombre;
+        }
     }
 }
