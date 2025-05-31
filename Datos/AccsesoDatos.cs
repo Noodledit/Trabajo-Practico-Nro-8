bb30ff6e55c750a74630f9acb2bc7a43913726a3
Noodledit <Nicolas.White.py@proton.me>
2025-05-31 14:15:52 -0300

    Dantes Commit - Definicion de clase AccesoDatos

--- Datos/AccsesoDatos.cs
+++ Datos/AccsesoDatos.cs
@@ -3,10 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
 
 namespace Datos
 {
-    internal class AccsesoDatos
+    public class AccsesoDatos
     {
+        private string connectionString = "Data Source=localhost;Initial Catalog=BDSucursales;Integrated Security=True";
+
+        private SqlConnection connection()
+        {
+            try
+            {
+                SqlConnection cnxn = new SqlConnection(connectionString);
+                cnxn.Open();
+                return cnxn;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        private SqlCommand sqlCommand (string query, SqlConnection conexion)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand(query, conexion);
+                return cmd;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public int EjecutarConsulta(string query)
+        {
+            try
+            {
+                using (SqlCommand cmd = sqlCommand(query, connection()))
+                {                                        
+                    if (cmd == null) return -1;
+                    int FilasAfectadas = cmd.ExecuteNonQuery();
+                    return FilasAfectadas;
+                }
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        public SqlDataAdapter EjecutarConsultaDataAdapter(string query)
+        {
+            try
+            {
+                using (SqlCommand cmd = sqlCommand(query, connection()))
+                {
+                    if (cmd == null) return null;
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable tabla = new DataTable();
+                    adapter.Fill(tabla);
+                    return adapter;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
     }
 }
